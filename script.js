e039ee3241b948b468766fbd01c4a70b86b4e242
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-17 21:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function conandAlert(){
+const okey = document.getElementById("ok");
+const canCel = document.getElementById("cancel");
+ 
+
+	okey.addEventListener("click",function(){
+		 const configconfirmed = confirm("Do you want to proceed?");
+		if(configconfirmed)
+		alert("You clicked OK. Proceeding...");
+		else{
+			alert("You clicked Cancel. Exiting...");
+		}
+	});
+}
+conandAlert();
